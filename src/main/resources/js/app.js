86387939a68898feb05d13ea5e02cff5dca497f6
Shawn Daniels <SGDaniels08@gmail.com>
2020-07-20 13:16:49 -0400

    JS linked up with Java, can generate HTML; design api-helper functions (fetchers)

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,3 +1,12 @@
 import { fetchAllArtists } from "./api-helper.js"
 
 console.log(fetchAllArtists());
+
+const contentContainer = document.createElement("main");
+contentContainer.classList.add("content__container");
+contentContainer.innerHTML = "IS THE JS WORKING?";
+
+const body = document.querySelector("body");
+
+body.appendChild(contentContainer);
+
