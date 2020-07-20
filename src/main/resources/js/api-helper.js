a4d55f1aa2bef99be282ff9d5552a6184b96d2fc
Shawn Daniels <SGDaniels08@gmail.com>
2020-07-20 13:53:22 -0400

    Homepage created with JS

--- src/main/resources/js/api-helper.js
+++ src/main/resources/js/api-helper.js
@@ -1,9 +1,46 @@
-
 /* All fetch functions */
 
 const fetchAllArtists = async () => {
-    return fetch("http://localhost:8080/api/artists")
-        .then(response => response.json())
-}
+  return fetch("http://localhost:8080/api/artists").then((response) =>
+    response.json()
+  );
+};
 
-export {fetchAllArtists}
+const fetchSingleArtist = async (artistId) => {
+  return fetch(
+    `http://localhost:8080/api/artists/${artistId}`
+  ).then((response) => response.json());
+};
+
+const fetchAllAlbums = async () => {
+  return fetch("http://localhost:8080/api/albums").then((response) =>
+    response.json()
+  );
+};
+
+const fetchSingleAlbum = async (albumId) => {
+  return fetch(`http://localhost:8080/api/albums/${albumId}`).then((response) =>
+    response.json()
+  );
+};
+
+const fetchAllSongs = async () => {
+  return fetch(`http://localhost:8080/api/songs`).then(
+    (response) => response.json
+  );
+};
+
+const fetchSingleSong = async (songId) => {
+  return fetch(`http://localhost:8080/api/songs/${songId}`).then(
+    (response) => response.json
+  );
+};
+
+export {
+  fetchAllArtists,
+  fetchSingleArtist,
+  fetchAllAlbums,
+  fetchSingleAlbum,
+  fetchAllSongs,
+  fetchSingleSong,
+};
