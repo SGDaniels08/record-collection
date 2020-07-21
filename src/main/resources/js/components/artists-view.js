1e9672fd277000387f790dff837a259b697b8420
Shawn Daniels <SGDaniels08@gmail.com>
2020-07-21 14:21:13 -0400

    All Artists View roughly functional, needs work

--- src/main/resources/js/components/artists-view.js
+++ src/main/resources/js/components/artists-view.js
@@ -1,5 +1,6 @@
-const renderArtistsView = () => {
+import { fetchAllArtists } from "../api-helper.js";
 
+const renderArtistsView = () => {
 const contentContainer = document.querySelector("main");
 contentContainer.classList.add("artists__mainSection");
 
@@ -7,52 +8,27 @@ contentContainer.innerHTML = `
 <h2 class="artists__title">Artists</h2>
 <div class="artists__wrapper">
 `
-// For each artist
+fetchAllArtists().then((artists)=> {
+    
+    artists.forEach(artist => {
+      console.log(artist);
+      let singleArtistSection = document.createElement("figure");
+      singleArtistSection.classList.add="artists__singleArtistSection";
+      singleArtistSection.innerHTML = `
+      <a href="singleartistpage">
+      <img
+      class="artists__singleArtistPicture"
+      src="../static/images/artists/${artist.imagePath}"
+      alt="Picture of artist / band ${artist.artistName}of Montreal"
+      />
+      <figcaption class="artists__singleArtistName">${artist.artistName}</figcaption>
+      </a>
+      `
+    
+    contentContainer.appendChild(singleArtistSection);
+});
 
-<figure class="artists__singleArtistSection">
-  <img
-    class="artists__singleArtistPicture"
-    src="../static/images/artists/ofMontreal.jpg"
-    alt="Picture of artist / band of Montreal"
-  />
-  <figcaption class="artists__singleArtistName">of Montreal</figcaption>
-</figure>
-<figure class="artists__singleArtistSection"><a href="artist.html">
-  <img
-    class="artists__singleArtistPicture"
-    src="../static/images/artists/tomWaits.jpg"
-    alt="Picture of artist / band Tom Waits"
-  />
-  <figcaption class="artists__singleArtistName">Tom Waits</figcaption>
-</a></figure>
-<figure class="artists__singleArtistSection">
-  <img
-    class="artists__singleArtistPicture"
-    src="../static/images/artists/florence+TheMachine.jpg"
-    alt="Picture of artist / band Florence + the Machine"
-  />
-  <figcaption class="artists__singleArtistName">
-    Florence + the Machine
-  </figcaption>
-</figure>
-<figure class="artists__singleArtistSection">
-  <img
-    class="artists__singleArtistPicture"
-    src="../static/images/artists/spoon.jpg"
-    alt="Picture of artist / band Spoon"
-  />
-  <figcaption class="artists__singleArtistName">Spoon</figcaption>
-</figure>
-<figure class="artists__singleArtistSection">
-  <img
-    class="artists__singleArtistPicture"
-    src="../static/images/artists/stVincent.png"
-    alt="Picture of artist / band St. Vincent"
-  />
-  <figcaption class="artists__singleArtistName">St. Vincent</figcaption>
-</figure>
-</div>
-`
-}
+contentContainer.appendChild("</div>");
+})};
 
 export { renderArtistsView }
