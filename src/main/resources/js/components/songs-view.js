722a5bb3cfe666d576290e293cb1d0379bd1c6ac
Shawn Daniels <SGDaniels08@gmail.com>
2020-08-02 14:48:18 -0400

    Songs page functional; next fix Artist and Song views

--- src/main/resources/js/components/songs-view.js
+++ src/main/resources/js/components/songs-view.js
@@ -1,4 +1,4 @@
-import { fetchAllSongs, fetchAllArtists } from "../api-helper.js"
+import { fetchAllSongs, fetchAllArtists, fetchSongAlbum, fetchAlbumArtist } from "../api-helper.js"
 
 const renderSongsView = ()=> {
 
@@ -33,13 +33,15 @@ contentContainer.appendChild(songsAllSongs);
 fetchAllSongs().then((songs)=> {
   // sort songs
   songs.forEach((song)=> {
+    fetchSongAlbum(song.id).then((songAlbum)=> {
+      fetchAlbumArtist(songAlbum.id).then((songArtist)=> {
     const songsSong = document.createElement("li");
     songsSong.classList.add("songs__song");
-    songsSong.innerHTML = `${song.songName} -- `;
+    songsSong.innerHTML = `${song.songName} -- on ${songAlbum.albumName}, by ${songArtist.artistName}`;
 
     songsAllSongs.appendChild(songsSong);
   })
 })
-};
+})})};
 
 export {renderSongsView}
